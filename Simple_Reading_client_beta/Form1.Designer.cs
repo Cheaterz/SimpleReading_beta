66966d696fee2a1e9078fad49f5c5ab8b1d64ce1
Cheaterz <andrew.cheater@gmail.com>
2014-05-20 09:43:35 +0300

    Done uploading

--- Simple_Reading_client_beta/Form1.Designer.cs
+++ Simple_Reading_client_beta/Form1.Designer.cs
@@ -40,14 +40,16 @@
             this.webBrowser1 = new System.Windows.Forms.WebBrowser();
             this.panel1 = new System.Windows.Forms.Panel();
             this.tbText = new System.Windows.Forms.RichTextBox();
-            this.panel3 = new System.Windows.Forms.Panel();
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.plLogin = new System.Windows.Forms.Panel();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btLogin = new System.Windows.Forms.Button();
             this.label1 = new System.Windows.Forms.Label();
-            this.button2 = new System.Windows.Forms.Button();
+            this.tbPassword = new System.Windows.Forms.TextBox();
+            this.tbLogin = new System.Windows.Forms.TextBox();
             this.panel2.SuspendLayout();
             this.panel1.SuspendLayout();
-            this.panel3.SuspendLayout();
+            this.plLogin.SuspendLayout();
             this.SuspendLayout();
             // 
             // menuStrip1
@@ -161,59 +163,83 @@
             // 
             this.tbText.Location = new System.Drawing.Point(378, 206);
             this.tbText.Name = "tbText";
+            this.tbText.ReadOnly = true;
             this.tbText.Size = new System.Drawing.Size(100, 96);
             this.tbText.TabIndex = 5;
             this.tbText.Text = "";
             // 
-            // panel3
+            // plLogin
             // 
-            this.panel3.Controls.Add(this.button2);
-            this.panel3.Controls.Add(this.label1);
-            this.panel3.Controls.Add(this.textBox2);
-            this.panel3.Controls.Add(this.textBox1);
-            this.panel3.Location = new System.Drawing.Point(62, 159);
-            this.panel3.Name = "panel3";
-            this.panel3.Size = new System.Drawing.Size(493, 236);
-            this.panel3.TabIndex = 11;
+            this.plLogin.Controls.Add(this.label3);
+            this.plLogin.Controls.Add(this.label2);
+            this.plLogin.Controls.Add(this.btLogin);
+            this.plLogin.Controls.Add(this.label1);
+            this.plLogin.Controls.Add(this.tbPassword);
+            this.plLogin.Controls.Add(this.tbLogin);
+            this.plLogin.Location = new System.Drawing.Point(0, 144);
+            this.plLogin.Name = "plLogin";
+            this.plLogin.Size = new System.Drawing.Size(417, 440);
+            this.plLogin.TabIndex = 11;
+            this.plLogin.VisibleChanged += new System.EventHandler(this.plLogin_VisibleChanged);
             // 
-            // textBox1
+            // label3
             // 
-            this.textBox1.Location = new System.Drawing.Point(307, 214);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(100, 20);
-            this.textBox1.TabIndex = 0;
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(249, 273);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(45, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Пароль";
             // 
-            // textBox2
+            // label2
             // 
-            this.textBox2.Location = new System.Drawing.Point(307, 270);
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(100, 20);
-            this.textBox2.TabIndex = 1;
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(249, 220);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(38, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Логин";
+            // 
+            // btLogin
+            // 
+            this.btLogin.Location = new System.Drawing.Point(307, 368);
+            this.btLogin.Name = "btLogin";
+            this.btLogin.Size = new System.Drawing.Size(75, 23);
+            this.btLogin.TabIndex = 3;
+            this.btLogin.Text = "Войти";
+            this.btLogin.UseVisualStyleBackColor = true;
+            this.btLogin.Click += new System.EventHandler(this.btLogin_Click);
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(320, 316);
+            this.label1.Location = new System.Drawing.Point(304, 316);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.Size = new System.Drawing.Size(0, 13);
             this.label1.TabIndex = 2;
-            this.label1.Text = "label1";
             // 
-            // button2
+            // tbPassword
             // 
-            this.button2.Location = new System.Drawing.Point(307, 367);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(75, 23);
-            this.button2.TabIndex = 3;
-            this.button2.Text = "button2";
-            this.button2.UseVisualStyleBackColor = true;
+            this.tbPassword.Location = new System.Drawing.Point(307, 270);
+            this.tbPassword.Name = "tbPassword";
+            this.tbPassword.Size = new System.Drawing.Size(151, 20);
+            this.tbPassword.TabIndex = 1;
+            this.tbPassword.UseSystemPasswordChar = true;
+            // 
+            // tbLogin
+            // 
+            this.tbLogin.Location = new System.Drawing.Point(307, 214);
+            this.tbLogin.Name = "tbLogin";
+            this.tbLogin.Size = new System.Drawing.Size(151, 20);
+            this.tbLogin.TabIndex = 0;
             // 
             // Form1
             // 
+            this.AcceptButton = this.btLogin;
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(715, 584);
-            this.Controls.Add(this.panel3);
+            this.Controls.Add(this.plLogin);
             this.Controls.Add(this.panel1);
             this.Controls.Add(this.webBrowser1);
             this.Controls.Add(this.menuStrip1);
@@ -224,8 +250,8 @@
             this.panel2.ResumeLayout(false);
             this.panel2.PerformLayout();
             this.panel1.ResumeLayout(false);
-            this.panel3.ResumeLayout(false);
-            this.panel3.PerformLayout();
+            this.plLogin.ResumeLayout(false);
+            this.plLogin.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -245,11 +271,13 @@
         private System.Windows.Forms.Label lbDate;
         private System.Windows.Forms.Panel panel1;
         private System.Windows.Forms.RichTextBox tbText;
-        private System.Windows.Forms.Panel panel3;
-        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Panel plLogin;
+        private System.Windows.Forms.Button btLogin;
         private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.TextBox textBox2;
-        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox tbPassword;
+        private System.Windows.Forms.TextBox tbLogin;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
 
     }
 }
